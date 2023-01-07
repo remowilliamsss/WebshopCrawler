bb34bb0fd4595d7250cbe4e2f8351b7379f07fb2
Viktor Egorov <vitka318@ya.ru>
2023-01-07 21:36:22 +0400

    dto refactoring

--- src/main/java/ru/egorov/StoreCrawler/dto/search/SearchResultDto.java
+++ src/main/java/ru/egorov/StoreCrawler/dto/search/SearchResultDto.java
@@ -3,7 +3,6 @@ package ru.egorov.StoreCrawler.dto.search;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
-import ru.egorov.StoreCrawler.dto.search.FoundProductDto;
 
 import java.util.Collections;
 import java.util.List;
