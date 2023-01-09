3a924ce8c66264d80a14c2d75c80366094616df5
Viktor Egorov <vitka318@ya.ru>
2023-01-09 23:12:26 +0400

    CrawlerService refactoring

--- src/main/java/ru/egorov/StoreCrawler/service/CrawlerService.java
+++ src/main/java/ru/egorov/StoreCrawler/service/CrawlerService.java
@@ -1,31 +1,30 @@
 package ru.egorov.StoreCrawler.service;
 
 import lombok.RequiredArgsConstructor;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import ru.egorov.StoreCrawler.parser.StoreParser;
 
 import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class CrawlerService {
-    private static final Logger log = LoggerFactory.getLogger(CrawlerService.class);
     private final List<StoreParser> parsers;
     private final DispatcherService dispatcherService;
+    private final ExecutorService executorService = Executors.newFixedThreadPool(2);
 
     @Scheduled(cron = "@daily")
     public void crawl() {
         log.info("Crawling starts...");
 
         parsers.forEach(parser ->
-                // TODO: 13.12.2022 executorService. Никогда не работай с голыми тредами в spring-приложении
-            new Thread(() -> {// TODO: 13.12.2022 избыточные {} в однострочной лямбде
-                dispatcherService.getProductsService(parser.getStore())
-                    .updateProducts(parser
-                        .parseProducts());
-            }).start());
+                executorService.execute(() ->
+                        dispatcherService.getProductsService(parser.getStore())
+                                .updateProducts(parser.parseProducts())));
     }
 }
