54a0c2950d2a6828be52bead1e61b627d7f0c6f9
Viktor Egorov <107366919+remowilliamsss@users.noreply.github.com>
2023-03-12 17:19:46 +0300

    updated README

--- src/test/java/ru/egorov/StoreCrawler/ProviderTest.java
+++ src/test/java/ru/egorov/StoreCrawler/ProviderTest.java
@@ -14,6 +14,7 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 import ru.egorov.StoreCrawler.controller.ProductController;
 import ru.egorov.StoreCrawler.controller.handler.ControllerExceptionHandler;
+import ru.egorov.StoreCrawler.model.product.StoreType;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -24,11 +25,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @Sql(value = {"/search/create-products.sql"}, executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
 @Sql(value = {"/search/delete-products.sql"}, executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
 public class ProviderTest {
-    public static final String FIND_BY_STORE_URL = "/api/products";
-    public static final String SNEAKERHEAD = "sneakerhead";
-    public static final String FOOTBOX = "footbox";
-    public static final String MESSAGE = "message";
-    public static final String SOME_STRING = "some string";
+    public static final String URL = "/api/products";
 
     @Autowired
     private WebApplicationContext webApplicationContext;
@@ -42,8 +39,8 @@ public class ProviderTest {
 
     @Test
     public void gainSneakerheadProducts() throws Exception {
-        this.mockMvc.perform(get(FIND_BY_STORE_URL)
-                        .param(ProductController.STORE, SNEAKERHEAD))
+        this.mockMvc.perform(get(URL)
+                        .param(ProductController.STORE, StoreType.sneakerhead.toString()))
                 .andExpect(status()
                         .isOk())
                 .andExpect(jsonPath("products[4]")
@@ -54,8 +51,8 @@ public class ProviderTest {
 
     @Test
     public void gainSneakerheadProductsWithPagination() throws Exception {
-        this.mockMvc.perform(get(FIND_BY_STORE_URL)
-                        .param(ProductController.STORE, SNEAKERHEAD)
+        this.mockMvc.perform(get(URL)
+                        .param(ProductController.STORE, StoreType.sneakerhead.toString())
                         .param(ProductController.PAGE, "0")
                         .param(ProductController.SIZE, "2"))
                 .andExpect(status()
@@ -68,8 +65,8 @@ public class ProviderTest {
 
     @Test
     public void gainFootboxProducts() throws Exception {
-        this.mockMvc.perform(get(FIND_BY_STORE_URL)
-                        .param(ProductController.STORE, FOOTBOX))
+        this.mockMvc.perform(get(URL)
+                        .param(ProductController.STORE, StoreType.footbox.toString()))
                 .andExpect(status()
                         .isOk())
                 .andExpect(jsonPath("products[4]")
@@ -80,8 +77,8 @@ public class ProviderTest {
 
     @Test
     public void gainFootboxProductsWithPagination() throws Exception {
-        this.mockMvc.perform(get(FIND_BY_STORE_URL)
-                        .param(ProductController.STORE, FOOTBOX)
+        this.mockMvc.perform(get(URL)
+                        .param(ProductController.STORE, StoreType.footbox.toString())
                         .param(ProductController.PAGE, "1")
                         .param(ProductController.SIZE, "2"))
                 .andExpect(status()
@@ -94,27 +91,27 @@ public class ProviderTest {
 
     @Test
     public void gainProductsFromNotSupportedStore() throws Exception {
-        this.mockMvc.perform(get(FIND_BY_STORE_URL)
-                        .param(ProductController.STORE, SOME_STRING))
+        this.mockMvc.perform(get(URL)
+                        .param(ProductController.STORE, "some string"))
                 .andExpect(status()
                         .isNotFound())
                 .andExpect(content()
                         .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(jsonPath(MESSAGE)
+                .andExpect(jsonPath("message")
                         .value(ControllerExceptionHandler.STORE_NOT_SUPPORTED));
     }
 
     @Test
     public void gainProductsFromNotSupportedStoreWithPagination() throws Exception {
-        this.mockMvc.perform(get(FIND_BY_STORE_URL)
-                        .param(ProductController.STORE, SOME_STRING)
+        this.mockMvc.perform(get(URL)
+                        .param(ProductController.STORE, "some string")
                         .param(ProductController.PAGE, "0")
                         .param(ProductController.SIZE, "2"))
                 .andExpect(status()
                         .isNotFound())
                 .andExpect(content()
                         .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(jsonPath(MESSAGE)
+                .andExpect(jsonPath("message")
                         .value(ControllerExceptionHandler.STORE_NOT_SUPPORTED));
     }
 }
